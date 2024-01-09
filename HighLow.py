59d253eb725607c5b76dca9d66a35a8f0e5142d2
Aneesha-Vangavolu <127774292+Aneesha-Vangavolu@users.noreply.github.com>
2024-01-09 15:58:40 -0500

    removed the comment and print

--- HighLow.py
+++ HighLow.py
@@ -1,8 +1,8 @@
-# This project is a game called Highlow
+
 import data
 import random
 from art import high_low,vs1
-print("Welcome to HighLow Game! This is more fun!")
+
 computer=None
 versus=None
 def guess():
